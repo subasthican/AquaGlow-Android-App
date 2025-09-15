af212fc157c0ee45753d589cc87566ecf7966987
subasthican <manoharansubasthican@gmail.com>
2025-09-15 17:57:36 +0530

    home screen addeded

--- app/src/main/java/com/example/aquaglow/MainActivity.kt
+++ app/src/main/java/com/example/aquaglow/MainActivity.kt
@@ -3,18 +3,39 @@ package com.example.aquaglow
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.nav_host_container, HomeFragment())
+                .commit()
+        }
+
+        val bottom = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottom.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_home -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.nav_host_container, HomeFragment())
+                        .commit()
+                    true
+                }
+                R.id.nav_habits -> {
+                    // TODO: open Habits screen fragment/activity
+                    true
+                }
+                R.id.nav_mood -> {
+                    // TODO: open Mood screen fragment/activity
+                    true
+                }
+                else -> false
+            }
         }
     }
 }
